b1b2e97200834f5c86c5fdad5a24b5eb85d4ae8d
Flakky <badbadmin@gmail.com>
2020-12-08 00:09:13 +0300

    Backtest mode

--- Trader.py
+++ Trader.py
@@ -4,6 +4,12 @@ import Position
 class Trader:
     openedPosition = None
     closedPositions = []
+    tradeInProgress = False
+    backtest = {
+    	"testMode": False,
+    	"tradeData": {},
+    	"testCurrentTime": None
+    }
     
     def __init__(self, strategy, budget, position_max_value):
     	self.strategy = strategy
@@ -15,14 +21,14 @@ class Trader:
 		if(strategy is None):
 			return
 		
-		tradeData = [0]
+		tradeData = self.recieveTradeData()
 		
 		if(openedPosition is not None):
 			if(strategy.shouldClosePosition(tradeData, openedPosition)):
 				closePosition()
 		else:
 			if(strategy.shouldOpenPosition(tradeData)):
-				openValue = tradeData[-1]
+				openValue = tradeData[-1].value
 				position = Position(
 					0, 
 					openValue,
@@ -50,3 +56,23 @@ class Trader:
         print("Position close: {p}".format(p=str(position)))
         
         self.openedPosition = None
+        
+        
+	def recieveTradeData(self):
+		tradeData = {"12.01.2020, 23.54": 125.534}
+
+
+	def start(self):
+		self.tradeInProgress = True
+		while self.tradeInProgress:
+			self.update()
+			sleep(5)
+			
+	
+	def stop(self):
+		self.tradeInProgress = False
+		
+		
+	def enableBacktestMode(self, test_trade_data):
+		self.backtest["testMode"] = True
+		self.backtest["tradeData"] = test_trade_data
