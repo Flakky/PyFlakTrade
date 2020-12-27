b3064b8d06493532cf025f79740ade53fdfb7fa7
Flakky <badbadmin@gmail.com>
2020-12-27 14:05:30 +0300

    positions limits by strategy

--- Strategies/Strategy.py
+++ Strategies/Strategy.py
@@ -2,6 +2,7 @@ import Position
 import StockValue
 import typing
 import matplotlib.axes
+import datetime
 
 
 class StrategyPlotter:
@@ -11,17 +12,41 @@ class StrategyPlotter:
 class Strategy:
 	trader = None #TODO: declare a type of Trader
 	plotter: StrategyPlotter.__class__ = StrategyPlotter
+	min_open_time: datetime.time = datetime.time(hour=8, minute=0)
+	max_open_time: datetime.time = datetime.time(hour=23, minute=58)
+	max_close_time: datetime.time = datetime.time(hour=23, minute=59)
+	position_time_limit: int = 60 # in minutes
+	
+	def __init__(self, **kwargs):
+		self.min_open_time = kwargs.get("min_open_time", self.min_open_time)
+		self.max_open_time = kwargs.get("max_open_time", self.max_open_time)
+		self.max_close_time = kwargs.get("max_close_time", self.max_close_time)
+		self.position_time_limit = kwargs.get("position_time_limit", self.position_time_limit)
 
 	def set_trader(self, trader):
 		self.trader = trader
 
 	def shouldOpenPosition(self, trade_data: typing.List[StockValue.StockValue]) -> bool:
+		last_value = trade_data[-1]
+		
+		if last_value.time_end.time() > self.max_open_time or last_value.time_end.time() < self.min_open_time:
+			return False 
+		
 		return True
 
 	def shouldClosePosition(self, trade_data: typing.List[StockValue.StockValue], position: Position.Position) -> bool:
 		if position is not None:
 			last_value = trade_data[-1]
-			if last_value.close_value < position.stop_loss or last_value.close_value > position.take_profit:
+			
+			if last_value.time_end.time() >= self.max_close_time:
+				return True
+				
+			position_minutes_delta = (last_value.time_end - position.open_time).total_seconds() / 60
+				
+			if position_minutes_delta >= self.position_time_limit:
+				return True
+				
+			if last_value.close_value <= position.stop_loss or last_value.close_value >= position.take_profit:
 				return True
 			else:
 				return False
