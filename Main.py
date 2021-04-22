3bbd71a05a994a55eaf1374eb12e848a66456d46
Flakky <badbadmin@gmail.com>
2021-04-23 00:28:31 +0300

    YFinance quote receive WIP

--- Main.py
+++ Main.py
@@ -2,9 +2,11 @@ import TradeSystem
 from Trader import Trader
 from TradeProviders.TradeProvider_Test import TradeProviderTest
 from QuoteProviders.QuoteProvider_Generator import QuoteProviderGenerator
+from QuoteProviders.QuoteProvider_YFinance import QuoteProviderYFinance
 from Strategies.MoveMeanProto.MoveMeanProtoStrategy import StrategyMoveMeanProto
 from Strategies.Strategy import Backtest
 from datetime import  datetime, timedelta
+
 
 TradeSystem.init()
 
@@ -12,15 +14,19 @@ tickers = ["AAPL", "AMD", "M"]
 
 backtest = Backtest()
 strategy = StrategyMoveMeanProto(
-	backtest=backtest
+	backtest=backtest,
+	update_period=timedelta(minutes=1)
 )
 trade_provider = TradeProviderTest(1000)
-quote_provider = QuoteProviderGenerator(
-	datetime.now() - timedelta(days=20),
-	datetime.now(),
-	timedelta(seconds=5),
-	tickers
-)
+
+#quote_provider = QuoteProviderGenerator(
+#	datetime.now() - timedelta(days=20),
+#	datetime.now(),
+#	timedelta(minutes=1),
+#	tickers
+#)
+
+quote_provider = QuoteProviderYFinance(tickers)
 
 trader = Trader(
 	strategy,
--- QuoteProviders/QuoteProvider_YFinance.py
+++ QuoteProviders/QuoteProvider_YFinance.py
@@ -3,40 +3,55 @@ from QuoteRequest import QuoteRequest
 from datetime import datetime, timedelta
 import yfinance
 from pandas import DataFrame
+from typing import Dict, List
 
 class QuoteProviderYFinance(QuoteProvider):
+	quotes: Dict[str, DataFrame] = {}
+	
+	def __init__(self, tickers: List[str]):
+		today = datetime.now()
+		
+		for ticker in tickers:
+			
+			ticker_stocks: DataFrame = None
+	
+			for week in range(int(30/7)):
+	
+				start = today - timedelta(days=min((week+1) * 7, 30))
+				end = today - timedelta(days=(week * 7))
+	
+				print("Downloading {ticker} stock from {start} to {end} with interval {interval}".format(
+					start=str(start.date()), 
+					end=str(end.date()), 
+					interval="1m",
+					ticker=ticker
+				))
+					
+				stocks_data = yfinance.download(
+					ticker,
+					start=str(start.date()),
+					end=str(end.date()),
+					interval="1m"
+				)
+				
+				if ticker_stocks is None:
+					ticker_stocks = stocks_data
+				else:
+					ticker_stocks.append(stocks_data)
+					
+			ticker_stocks = ticker_stocks.tz_convert(None)
+					
+			print(ticker_stocks.index)
+			print(datetime(year=2021, month=4, day=3, hour=5))
+			
+			self.quotes[ticker] = stocks_data
+		
 	
 	def read_quotes(self, request: QuoteRequest) -> DataFrame:
-		out_stocks: DataFrame = DataFrame()
+		data = self.quotes[request.ticker]
 
-		today = datetime.now()
-
-		for week in range(int(30/7)):
-
-			start = today - timedelta(days=min((week+1) * 7, 30))
-			end = today - timedelta(days=(week * 7))
-			
-			interval = ""
-			if request.period == 1:
-				interval = "1s"
-			if request.period == 5:
-				interval = "5s"
-			if request.period == 60:
-				interval = "1m"
-
-			print("Downloading stock from {start} to {end} with interval {interval}".format(
-				start=str(start.date()), 
-				end=str(end.date()), 
-				interval=interval)
-			)
-				
-			data = yfinance.download(
-				request.ticker,
-				start=str(start.date()),
-				end=str(end.date()),
-				interval=interval
-			)
-
-			out_stocks.append(data)
+		mask = (data.index > request.start) & (data.index <= request.end)
+		
+		out_stocks = data.loc[mask]
 
 		return out_stocks
